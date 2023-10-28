1626df5f6d84a502b0294db6b0aaccbeff274505
Abr <59525997+Abr009@users.noreply.github.com>
2023-10-28 10:53:46 +0530

    Update hide_icon.ino

--- hide_icon.ino
+++ hide_icon.ino
@@ -1,5 +1,5 @@
 //This DigiKeyboard Script takes a screenshot of the desktop, sets it as the wallpaper and hides the desktop icons.
-//Work with Windows XP(?)/Vista(?)/7/8/8.1/10 etc.
+//Work with Windows XP/Vista/7/8/8.1/10 etc.
 
 #include "DigiKeyboard.h"
 void setup() {
